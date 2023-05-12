441370f4cbc2692f89093a9eda755a7d2e32c6fe
LeAwesomeGuy <adam.n.kertz@gmail.com>
2023-05-11 21:15:45 -0500

    Removed old redundant files

--- EmployeeSQL/Old Files/1986 Employees.sql
+++
@@ -1,5 +0,0 @@
---List the first name, last name, and hire date for the employees who were hired in 1986
-SELECT last_name, first_name, hire_date
-FROM "Employees"
-WHERE hire_date LIKE '%1986'
-ORDER BY last_name
--- EmployeeSQL/Old Files/Employee Salary Info.sql
+++
@@ -1,5 +0,0 @@
-SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
-FROM "Employees" AS e
-LEFT JOIN "Salaries" AS s
-ON e.emp_no = s.emp_no
-ORDER BY e.emp_no;
